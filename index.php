48625dbf1b10dd7ab508581f15861f9830e6fe03
alkhulaqiw <83186291+alkhulaqiw@users.noreply.github.com>
2025-12-29 16:21:06 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+<head>
+<meta charset="UTF-8">
+<title>موقع وليد الخلاقي</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="google-adsense-account" content="ca-pub-7771092438278031">
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7771092438278031"
+crossorigin="anonymous"></script>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>موقع وليد الخلاقي</h1>
+<p>نظام إدارة محتوى كامل.</p>
+<a href="admin/login.php">لوحة التحكم</a>
+</body>
+</html>
